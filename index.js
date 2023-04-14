6ca8f3f2fad46f57cce4fc81fb5de3c84447dadb
Sergio Múnera <sergiomunera17@gmail.com>
2023-04-13 21:04:08 -0500

    pagina principal

---
+++ index.js
@@ -0,0 +1,7 @@
+let elemento1 = document.querySelector(".boton1")
+let elemento2 = document.querySelector(".container")
+
+elemento1.addEventListener("click", function(){
+    // alert("diste click")
+    elemento1.style.backgroundColor= "red";
+})
